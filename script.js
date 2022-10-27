588cbc445cc367b285411e5b597529d8a3adb730
Natalia Rybakov <78943679+nataliarybakov@users.noreply.github.com>
2022-10-27 14:00:22 +1100

    Validate HTML, CSS and JS files.

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 document.querySelector('.menu').addEventListener('click',() => {
-    document.querySelectorAll('.target').forEach((item) => {
-        item.classList.toggle('change')
-    })
-})
+document.querySelectorAll('.target').forEach((item) => {
+        item.classList.toggle('change');
+    });
+});
 
+const icons = document.querySelectorAll('.section-1-icons i');
 
-
-
-const icons = document.querySelectorAll('.section-1-icons i')
-
-let i = 1
+let i = 1;
 
 setInterval(() => {
-i++
+i++;
 
-const icon = document.querySelector('.section-1-icons .change')
+const icon = document.querySelector('.section-1-icons .change');
 
-icon.classList.remove('change')
+icon.classList.remove('change');
 
 if(i > icons.length) {
-    icons[0].classList.add('change')
-    i = 1
+    icons[0].classList.add('change');
+    i = 1;
 } else {
-    icon.nextElementSibling.classList.add('change')
+    icon.nextElementSibling.classList.add('change');
 }
 
-},4000)
+},4000);
